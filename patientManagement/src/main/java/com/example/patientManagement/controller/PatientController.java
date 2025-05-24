b08a7eda2f7a914f5a98156dc3ff4b8b38cc018e
Hiruni.N <nawanjuhiru2000@gmail.com>
2025-05-24 14:45:08 +0530

    first

--- patientManagement/src/main/java/com/example/patientManagement/controller/PatientController.java
+++ patientManagement/src/main/java/com/example/patientManagement/controller/PatientController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/patients")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PatientController {
     private final PatientService service;
 
